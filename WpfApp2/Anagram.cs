f1f50e762408e4c07b61dab58f0a4ff44510896a
tarasinski.dawid <tarasinski.dawid@STACJA105-09>
2023-10-18 14:14:56 +0200

    Dodaj pliki projektów.

---
+++ WpfApp2/Anagram.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    internal class Anagram : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        string text1; 
+        string text2;
+        public string Text1
+        {  get 
+            { 
+                return text1; 
+            } 
+           set
+            {  
+                text1 = value; 
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("Text1"));
+            } 
+        }
+       public string Text2
+        {
+            get
+            { 
+                return text2; 
+            }
+            set
+            {
+                text2 = value;
+                if(PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("Text2"));
+            }
+        }
+        public Anagram()
+        {
+            Text1 = "Dawid";
+            Text2 = "Tarasinski";
+        }
+        public bool sprawdźanagramy
+    }
+}
